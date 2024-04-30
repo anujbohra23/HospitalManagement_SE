f853662006be809705fce4a90cd93462b1e89d7c
anujbohra23 <anujbohra10@gmail.com>
2024-04-30 10:16:11 +0530

    index page

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-#Tushar Borole
-#Python 2.7
+# Python 2.7
 
-from flask import Flask,send_from_directory,render_template
+from flask import Flask, send_from_directory, render_template
 from flask_restful import Resource, Api
 from package.patient import Patients, Patient
 from package.doctor import Doctors, Doctor
@@ -10,26 +9,27 @@ from package.common import Common
 import json
 
 
-with open('config.json') as data_file:
+with open("config.json") as data_file:
     config = json.load(data_file)
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path="")
 api = Api(app)
 
-api.add_resource(Patients, '/patient')
-api.add_resource(Patient, '/patient/<int:id>')
-api.add_resource(Doctors, '/doctor')
-api.add_resource(Doctor, '/doctor/<int:id>')
-api.add_resource(Appointments, '/appointment')
-api.add_resource(Appointment, '/appointment/<int:id>')
-api.add_resource(Common, '/common')
+api.add_resource(Patients, "/patient")
+api.add_resource(Patient, "/patient/<int:id>")
+api.add_resource(Doctors, "/doctor")
+api.add_resource(Doctor, "/doctor/<int:id>")
+api.add_resource(Appointments, "/appointment")
+api.add_resource(Appointment, "/appointment/<int:id>")
+api.add_resource(Common, "/common")
 
 # Routes
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True,host=config['host'],port=config['port'])
+if __name__ == "__main__":
+    app.run(debug=True, host=config["host"], port=config["port"])
